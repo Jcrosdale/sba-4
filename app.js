803039cc603b38d9c401dd60793e83b4e77568d0
Janelle Crosdale <janelle@Janelles-MacBook-Air.local>
2026-09-23 23:54:58 -0400

    Adding logic to update, overdue, and filter functions

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ const categoryInput = document.getElementById('categoryInput');
 const deadlineInput = document.getElementById('deadlineInput');
 const statusInput = document.getElementById('statusInput');
 const addTaskButton = document.getElementById('addTaskButton');
+let option = document.createElement("option");
 
 //Use an array to store tasks, each represented as an object.
 let taskList = [];
-
 let newTaskId = 1;
+
 //Write functions to add tasks, update task status, check overdue tasks, and filter tasks.
-function addTask(){
-    
+
+function addTask() {    
     const task = {
         id: newTaskId,
         name: taskInput.value,
@@ -31,18 +32,23 @@ function addTask(){
     };
 
     newTaskId++;
-    
     taskList.push(task);
-
     displayTasks();
 };
 
-function updateTask() {
-
+function updateTask(taskId, newStatus) {
+    const task = taskList.find(task => task.id === taskId);
+    task.status = newStatus;
 };
 
 function overdueTask() {
-
+    for (let task of taskList) {
+        const today = new Date();
+        const deadline = new Date(task.deadline);
+        if (deadline < today && task.status !== 'Completed'){
+            task.status='Overdue';
+        }
+    }
 };
 
 function filterTask() {
@@ -58,9 +64,32 @@ function displayTasks() {
     
     for (let task of taskList) {
         let listItem = document.createElement("li");
-        listItem.textContent = `${task.name} | ${task.category} | ${task.deadline} | ${task.status}`;
+        let selectItem = document.createElement("select");
+        let status1 = document.createElement("option");
+        let status2 = document.createElement("option");
+        let status3 = document.createElement("option");
+        status1.textContent = 'Not Started';
+        status2.textContent = 'In Progress';
+        status3.textContent = 'Completed';
+        selectItem.appendChild(status1);
+        selectItem.appendChild(status2);
+        selectItem.appendChild(status3);
+
+        listItem.textContent = `${task.id} | ${task.name} | ${task.category} | ${task.deadline} | ${task.status}`;
+
+        listItem.appendChild(selectItem);
         tasks.appendChild(listItem);
+
+        selectItem.addEventListener('change', function() {
+            updateTask(task.id, selectItem.value);
+
+            displayTasks();
+
+        });
     }
 }
+
+overdueTask();
+displayTasks();
 
 //Implement local storage to persist task data.
